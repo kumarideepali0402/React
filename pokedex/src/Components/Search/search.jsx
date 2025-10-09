5dc846ff56ca9d371269a1a7ba5d799a9661c98a
Deepali <deepalikumari62004@gmail.com>
2025-10-09 20:46:08 +0530

    implemented search feature and added sebouncing in it

--- pokedex/src/Components/Search/search.jsx
+++ pokedex/src/Components/Search/search.jsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react"
 import "./Search.css"
+import { useNavigate } from "react-router-dom";
 export default function Search() {
+    const [searchTerm, setSearchTerm] = useState("");
+    const navigate = useNavigate();
+
+    
+        useEffect(()=>{
+           const timeout =  setTimeout(()=>{
+               if(searchTerm.trim().length === 0) {
+                   navigate('/')
+               }
+               else if(searchTerm.trim().length > 0){
+                   navigate(`/pokemon/${searchTerm.toLowerCase()}`)
+               }
+                
+            },1000)
+            return ()=>clearInterval(timeout);
+
+        }
+     ,[searchTerm, navigate])
+        
+    
     return(
-        <>
-        <input type="text" name="search" placeholder="Search Pokedex..." className="search-bar"/>
-        </>
+        <div>
+        <input type="text" name="search" placeholder="Search Pokedex..." className="search-bar" value={searchTerm}
+        onChange={(e)=>setSearchTerm(e.target.value)}/>
+        </div>
     )
 }
